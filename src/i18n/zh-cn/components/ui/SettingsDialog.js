a991d7559203c9f29162d37191878baa64e089c2
tanknee <nee@tanknee.cn>
2020-09-20 14:20:00 +0800

    20200920 1. 修复了翻译缺失的问题

--- src/i18n/zh-cn/components/ui/SettingsDialog.js
+++ src/i18n/zh-cn/components/ui/SettingsDialog.js
@@ -6,5 +6,9 @@ export default {
   'en-us': '英语 (美国)',
   'zh-cn': '中文 (简体)',
   darkMode: '暗黑模式',
-  markdownOnly: '只显示markdown笔记'
+  markdownOnly: '只显示markdown笔记',
+  imageUploadService: '图片上传服务',
+  wizOfficialImageUploadService: '为知官方图片上传服务',
+  customWebUploadService: '自定义Web图片服务器',
+  smmsImageUploadService: 'SMMS图片上传服务'
 }
--- src/i18n/zh-cn/components/ui/index.js
+++ src/i18n/zh-cn/components/ui/index.js
@@ -1,9 +1,12 @@
 import LoginDialog from 'src/i18n/zh-cn/components/ui/LoginDialog'
 import SettingsDialog from 'src/i18n/zh-cn/components/ui/SettingsDialog'
+import CategoryDialog from 'src/i18n/zh-cn/components/ui/CategoryDialog'
 import NoteItemContextMenu from 'src/i18n/zh-cn/components/ui/NoteItemContextMenu'
-
+import ImageUploadServiceDialog from 'src/i18n/zh-cn/components/ui/ImageUploadServiceDialog'
 export default {
   ...LoginDialog,
   ...SettingsDialog,
-  ...NoteItemContextMenu
+  ...CategoryDialog,
+  ...NoteItemContextMenu,
+  ...ImageUploadServiceDialog
 }
